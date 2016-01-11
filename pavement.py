e14615f38d2ac78bfe2c315e5819b6c2281ed7f3
Brad Miller <bonelake@mac.com>
2016-01-10 18:56:12 -0800

    add basecourse to configuration

--- pavement.py
+++ pavement.py
@@ -28,6 +28,8 @@ options(
             'course_url':master_url,
             'use_services': 'true',
             'python3': 'true',
+            'dburl': 'postgresql://bmiller@localhost/runestone',
+            'basecourse': 'pythonds',
         }
     )
 )
