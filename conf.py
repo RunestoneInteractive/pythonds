ee3c9747291e1ebd439a525304be35b69f987315
Brad Miller <brad@runestoneinteractive.com>
2022-02-22 20:15:12 +0000

    add shelf section

--- conf.py
+++ conf.py
@@ -170,6 +170,7 @@ html_theme_path = [pkg_resources.resource_filename('runestone', 'common/project_
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
 html_title = 'Problem Solving with Algorithms and Data Structures 3rd edition'
+shelf_section = "Data Structures"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 html_short_title ='Problem Solving with Algorithms and Data Structures 3rd edition'
